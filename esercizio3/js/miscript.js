6aae721e754cadc844278f43c543d31971a45ce9
Alessandro <51783420+xamanek@users.noreply.github.com>
2019-11-04 15:22:05 +0100

    Add files via upload

---
+++ esercizio3/js/miscript.js
@@ -0,0 +1,15 @@
+function asterisco(numeroast)
+{
+	
+	for (i=1;i<=numeroast;i++){
+		document.write("*")
+	}
+}
+
+function piramide()
+{
+	for (j=1;j<=numero;j++){
+		asterisco(j);
+		document.write("<br>");
+	}
+}
